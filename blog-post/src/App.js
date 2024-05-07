adc6834b11f83c133cde9ad8cc815cb425a62b84
Kitui Kelly <kituikelly@gmail.com>
2024-05-08 02:50:30 +0300

    initial commit

--- blog-post/src/App.js
+++ blog-post/src/App.js
@@ -1,9 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Blogs from "./Blogs";
+import BlogList from "./components/BlogList";
+import NavBar from "./components/NavBar";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <Blogs />;
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <BlogList />
+    </>
+  );
 }
 
 export default App;

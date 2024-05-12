2e7fa85c3ec3c66e5152038945f1b6cbb41bbcb4
lilianmueninzioki <liliannzioki@gmail.com>
2024-05-12 12:17:58 +0300

    blog-card

--- blog-post/src/components/BlogCard.js
+++ blog-post/src/components/BlogCard.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './img.css';
-import BlogModal from './BlogModal';
-import './BlogCard.css'
+import './BlogCard.css';
+import BlogModal from './BlogModal'; // Import BlogModal
 
 const truncateText = (text, length) => {
   return text.length > length ? `${text.substring(0, length)}...` : text;
@@ -19,38 +18,37 @@ const BlogCard = ({ blogs }) => {
     setSelectedBlog(null);
   };
 
-  const deleteBlogPost = async (blog) => {
-    try {
-      const response = await axios.delete(
-        `http://localhost:8002/posts/${blog.id}`
-      );
-      console.log(response.data);
-      // Reload the page to reflect the deleted blog post
-      window.location.reload();
-    } catch (error) {
-      console.error('Error deleting blog post:', error);
-    }
-  };
+  // Remove deleteBlogPost if not used
+  // const deleteBlogPost = async (blog) => {
+  //   try {
+  //     const response = await axios.delete(
+  //       `http://localhost:8002/posts/${blog.id}`
+  //     );
+  //     console.log(response.data);
+  //     // Reload the page to reflect the deleted blog post
+  //     window.location.reload();
+  //   } catch (error) {
+  //     console.error('Error deleting blog post:', error);
+  //   }
+  // };
 
   return (
     <div className="blog-container">
-      <div className="blog-list">
-        {blogs.map((blog) => (
-          <div key={blog.id} className="blog-post">
-            <h3>{blog.title}</h3>
-            {blog.image && (
-              <img className="blog-image" src={blog.image} alt={blog.title} />
-            )}
-            <p>
-              {truncateText(blog.body, 100)}{' '}
-              <button onClick={() => handleOpenModal(blog)}>Read More</button>
-            </p>
-            <p className="category">{blog.category}</p>
-            <p className= "category">{blog.author}</p>
-            {/* <button onClick={() => deleteBlogPost(blog)}>Delete post</button> */}
-          </div>
-        ))}
-      </div>
+      {blogs.map((blog) => (
+        <div key={blog.id} className="blog-post">
+          <h3>{blog.title}</h3>
+          {blog.image && (
+            <img className="blog-image" src={blog.image} alt={blog.title} />
+          )}
+          <p>
+            {truncateText(blog.body, 100)}{' '}
+            <button onClick={() => handleOpenModal(blog)}>Read More</button>
+          </p>
+          <p className="category">{blog.category}</p>
+          <p className="author">{blog.author}</p>
+          {/* <button onClick={() => deleteBlogPost(blog)}>Delete post</button> */}
+        </div>
+      ))}
       <BlogModal
         blog={selectedBlog}
         isVisible={Boolean(selectedBlog)}
